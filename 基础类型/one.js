2cd9371741010a4f258182742d175192fb1ced83
guixiang <276935962.qq.com>
2019-09-10 18:57:19 +0800

    基础类型

---
+++ 基础类型/one.js
@@ -0,0 +1,46 @@
+"use strict";
+// interface person {
+//   firstName: string;
+//   lastName: string;
+// }
+// function greeter(person: person): string {
+//   return 'Hello, ' + person.firstName + ',' + person.lastName;
+// }
+// 布尔值
+// let isFalse: boolean = false;
+// let isTrue: boolean = true;
+// 数字
+// let decLiteral: number = 6;
+// 字符串
+// let user = { firstName: 'Jane', lastName: 'User' };
+// document.body.innerHTML = greeter(user);
+// console.log(user);
+// let userName: string = "Gen"
+// let sentence: string = `Hello, My name is ${userName}`
+// console.log(sentence);
+// 数据类型
+// let list: number[] = [1,2,3,4]
+// let lists: Array<number> = [1,2,3,4]
+// console.log(list);
+// console.log(lists);
+// 元组类型
+// let x:[string,number];
+// x = ['Hello',10];
+// console.log(x);
+// 枚举类型
+// enum Color {Red="red", Green="red", Blue="red"}
+// let c: Color = Color.Red;
+// console.log(c);
+// Any类型
+// let any: any = 4;
+// console.log(any);
+// any = 'my'
+// console.log(any);
+// void 类型
+// function warnUser(): void {
+//   console.log("This is my warning message");
+// }
+// let unusable: void = undefined;
+// null和undefined
+// let a: null = null
+// let b: string = ''
